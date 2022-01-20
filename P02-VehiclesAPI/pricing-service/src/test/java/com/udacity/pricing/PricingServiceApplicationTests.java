08f2fe6ad649db32ab90bd0b3907a5e7154ac982
Raphael G <raphaelgal@gmail.com>
2022-01-20 22:12:41 +0200

    Implemented tests of pricing service

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,32 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.api.PricingController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void testGetPrice() throws Exception {
+		mockMvc.perform(get("/services/price").param("vehicleId", "1"))
+				.andExpect(status().isOk());
 	}
 
+	@Test
+	public void testGetPriceWithInvalidId() throws Exception {
+		mockMvc.perform(get("/services/price").param("vehicleId", "500"))
+				.andExpect(status().isNotFound());
+	}
 }
